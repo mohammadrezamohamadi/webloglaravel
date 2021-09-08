fcc3e3092dead025548f27bdf797d66a8a043239
mohamad reza mohamadi <mrezamohamadi2259@gmail.com>
2021-09-08 19:34:04 +0430

    add login and register view

--- weblog/routes/web.php
+++ weblog/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('auth/login', function (){
+    return view('auth.login');
+});
+Route::get('auth/register', function () {
+    return view('auth.register');
+});
+
+
+
+
+
+
